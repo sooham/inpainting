737ff811eeca1fd8cff68d9bb8ac37e7f9f84c58
sooham <rafizsooham@gmail.com>
2016-05-16 20:31:33 -0400

    Xcode push

---
+++ inpainting/inpainting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  inpainting
+//
+//  Created by Sooham Rafiz on 2016-05-16.
+//  Copyright © 2016 Sooham Rafiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
