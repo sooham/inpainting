b655282e7ff390f51cdcadb73d14b9835e297b0e
sooham <rafizsooham@gmail.com>
2016-05-19 00:23:14 -0400

    computing gradient done

--- inpainting/inpainting.cpp
+++ inpainting/inpainting.cpp
@@ -20,6 +20,8 @@
 #include <vector>
 #include <sstream>
 
+#define WINDOW_RADIUS 4
+
 // takes in an image and the binary mask, adds the appropriate alpha channel
 // and returns the confidence integer map
 // mask is of type CV_8UC1 255 if target else 0
@@ -71,6 +73,9 @@ std::vector<std::vector<cv::Point>> *get_boundary(const cv::Mat &mask) {
 }
 
 
+
+
+
 // get a patch of size radius around Point p in Mat
 cv::Mat get_radius(cv::Mat image, cv::Point &p, int r=0) {
     // uses return value optimization
@@ -81,10 +86,18 @@ cv::Mat get_radius(cv::Mat image, cv::Point &p, int r=0) {
 }
 
 
+
+
+
+
+
+
 int main( int argc, const char** argv )
 {
     // TODO: set Xcode command line arguments to point to images
     
+    
+    // ---------------- getting image and mask string names -----------------------
     std::string in_image_file;
     std::string mask_file;
     
@@ -98,8 +111,11 @@ int main( int argc, const char** argv )
     
     std::cerr << "input filename: " << in_image_file << std::endl;
     std::cerr << "input mask: " << mask_file << std::endl;
+    std::cerr << std::endl;
     
-    cv::Mat img_in = cv::imread(in_image_file, -1);
+    
+    // ------------- image reading -------------------------
+    cv::Mat img_in = cv::imread(in_image_file, 1);
     cv::Mat mask = cv::imread(mask_file, 0);
     
     if (img_in.empty() || mask_file.empty()) {
@@ -112,13 +128,9 @@ int main( int argc, const char** argv )
         return -1;
     }
     
-    // convert img_in to 32F
+    // add an alpha channel to img_in
     img_in.convertTo(img_in, CV_32F);
-    
     img_in = img_in / 255.0f;
-    
-    mask = (mask == 0); // negate the mask to play well with find countours
-    
     
     std::cerr << "img_in aspects" << std::endl << std::endl;
     std::cerr << "\tchannels: " << img_in.channels() << std::endl;
@@ -132,7 +144,7 @@ int main( int argc, const char** argv )
     std::cerr << "\tdepth: " << mask.depth() << std::endl << std::endl;
     
     
-    // showing the loaded files
+    // --------------------- showing the loaded images ---------------------------
     
     cv::namedWindow("img_in");
     cv::imshow("img_in", img_in);
@@ -144,49 +156,128 @@ int main( int argc, const char** argv )
     cv::waitKey(0);
     cv::destroyWindow("mask");
     
-    // convert img_in to CIELab color space
-    cv::Mat img_in_cie;
-    cv::cvtColor(img_in, img_in_cie, cv::COLOR_BGR2Lab);
-    img_in_cie = img_in_cie / 255.0;
+    // convert img_in to grayscale
+    cv::Mat img_in_grayscale;
+    cv::cvtColor(img_in, img_in_grayscale, cv::COLOR_BGR2GRAY);
     
     double min, max;
-    cv::minMaxLoc(img_in_cie, &min, &max);
-    // show the img_in_cie
-    std::cerr << "img_in_cie aspects" << std::endl << std::endl;
-    std::cerr << "\tchannels: " << img_in_cie.channels() << std::endl;
-    std::cerr << "\tsize: " << img_in_cie.size << std::endl;
-    std::cerr << "\tdepth: " << img_in_cie.depth() << std::endl << std::endl;
-    std::cerr << "\tmin: " << min << std::endl;
-    std::cerr << "\tmax: " << max << std::endl;
+    cv::minMaxLoc(img_in_grayscale, &min, &max);
+    std::cerr << std::endl << "depth is: " << img_in_grayscale.depth() << std::endl;
+    std::cerr << "min is: " << min << std::endl;
+    std::cerr << "max is: " << max << std::endl;
+    std::cerr << std::endl;
     
-    cv::namedWindow("img_in_cie");
-    cv::imshow("img_in_cie", img_in_cie);
+    // convert img_in to CIELab color space
+    // TODO: should I divide by 255?
+    cv::Mat img_in_cie;
+    cv::cvtColor(img_in, img_in_cie, cv::COLOR_BGR2Lab);
+    
+    // show grayscale
+    cv::namedWindow("gray");
+    cv::imshow("gray", img_in_grayscale);
     cv::waitKey(0);
-    cv::destroyWindow("img_in_cie");
+    cv::destroyWindow("gray");
     
-    // TODO: I'm using single precision floating point, so alls good
+    // ---- assign confidence value to img_in pixels ---------
+    // done through mask (invert when getting boundary)
     
-    std::vector<std::vector<cv::Point>> *contours = get_boundary(mask);
+    // ---- get a full matrix of the x and y derivatives
+    cv::Mat dx, dy, magnitude;
+    cv::Sobel(img_in_grayscale, dx, -1, 1, 0, -1);
+    cv::Sobel(img_in_grayscale, dy, -1, 0, 1, -1);
+    cv::magnitude(dx, dy, magnitude);
     
-    // show the image as we continue the algorithm
+    std::cerr << std::endl << "mag depth is: " << magnitude.depth() << std::endl;
+    std::cerr << "min is: " << min << std::endl;
+    std::cerr << "max is: " << max << std::endl;
+    std::cerr << std::endl;
+    
+    // show the magnitude
+    cv::namedWindow("magnitude");
+    cv::imshow("magnitude", magnitude);
+    cv::waitKey(0);
+    cv::destroyWindow("magnitude");
+    
+    // ----------- run the algorithm ------------
+    // get source - all non target Points
+    cv::Mat source;
+    cv::findNonZero(mask, source);
+
+    // get boundary
+    std::vector<std::vector<cv::Point>> *contours = get_boundary((mask == 0));
+    
+    // assign each pixel on boundary a  priority
+    // done in through the mask 255 0 values
+    
+    // show the image as we compute the normal and gradient of each contour patch
     cv::namedWindow("Algorithm progress");
     
-    // iterate over the contours one by one
+    // iterate over the contours points one by one
     for (int i = 0; i < contours->size(); ++i) {
         std::vector<cv::Point> contour = (*contours)[i];
         for (int j = 0; j < contour.size(); ++j) {
-            // get the patch on a contour
-            std::cerr << "value at " << j << " is " <<contour[j] << std::endl;
-            cv::Mat patch = get_radius(img_in, contour[j], 2);
-            // draw the patch and point on img_in
-            cv::rectangle(img_in, cv::Rect(contour[j].x-2, contour[j].y-2, 5, 5), cv::Scalar(0,255,0));
-            cv::circle(img_in, contour[j], 0, cv::Scalar(0,0,255));
-            cv::imshow("Algorithm progress", img_in);
-            cv::waitKey(5);
+            cv::Mat img_in_clone = img_in.clone();
+            
+            // get the patch on point in img_in (used to find gradient in source regions)
+            cv::Mat patch_img_in = get_radius(img_in, contour[j], WINDOW_RADIUS);
+            
+            // get the patch on point in mask (used to indicate which area is target vs source)
+            cv::Mat patch_mask = get_radius(mask, contour[j], WINDOW_RADIUS);
+            
+            // get the gradient patch in the img_in
+            cv::Mat patch_magnitude = get_radius(magnitude, contour[j], WINDOW_RADIUS);
+            // set all target areas in magnitude to 0
+            cv::Mat occluded_magnitude;
+            cv::bitwise_and(patch_mask, patch_magnitude, occluded_magnitude);
+            
+            // go over the occluded_magnitude and get the Point of maximum
+            int nRows = occluded_magnitude.rows;
+            int nCols = occluded_magnitude.cols;
+            
+            if (occluded_magnitude.isContinuous()) {
+                nCols *= nRows;
+                nRows = 1;
+            }
+            
+            float max = 0;
+            int x = 0, y = 0;
+            uchar *input = occluded_magnitude.data;
+            for (int j = 0; j < nRows; ++j) {
+                for (int i = 0; i < nCols; ++i) {
+                    if (max < input[occluded_magnitude.step * j + i]) {
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+            
+            // get the dx and dy tuple
+            float gradient_x = dx.at<float>(x, y);
+            float gradient_y = dy.at<float>(x, y);
+            
+            cv::Vec2f gradient(gradient_x, gradient_y);
+            
+            // now what you have the gradient, you need to get the normal from the mask patch
+            
+            // compute the data value
+            
+            
+            // compute the confidence of the patch
+            
+            // compute the priority of the patch
+            
+            
+            // draw the patch and point on img_in_clone for view debugging
+            cv::rectangle(img_in_clone, cv::Rect(contour[j].x-WINDOW_RADIUS, contour[j].y-WINDOW_RADIUS, 2 * WINDOW_RADIUS + 1, 2 * WINDOW_RADIUS + 1), cv::Scalar(0,255,0));
+            // draw point
+            cv::circle(img_in_clone, contour[j], 0, cv::Scalar(0,0,255));
+            // now find the patch with the least distance in the source of CIE Lab colorspace
+            cv::imshow("Algorithm progress", img_in_clone);
+            cv::waitKey(1);
+            cv::destroyWindow("Algorithm progress");
         }
     }
     cv::waitKey(0);
-    cv::destroyWindow("Algorihtm progress");
     
     
     return 0;
